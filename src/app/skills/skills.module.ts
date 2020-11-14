6de899e079648dea71c7a3cf38d760e080eb0848
Leteinturier <adrienleteinturier@yahoo.fr>
2020-11-14 18:08:19 +0100

    add toaster in skills component

--- src/app/skills/skills.module.ts
+++ src/app/skills/skills.module.ts
@@ -2,16 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SkillsComponent } from "./skills.component";
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ToastaModule, ToastaService } from 'ngx-toasta';
 
 @NgModule({
     imports: [ 
         CommonModule,
         FormsModule,
-        ReactiveFormsModule
+        ReactiveFormsModule,
+        ToastaModule.forRoot()
     ],
     declarations: [ SkillsComponent ],
     exports: [ SkillsComponent ]
-})
+}) 
 export class SkillsModule {
 
 }
