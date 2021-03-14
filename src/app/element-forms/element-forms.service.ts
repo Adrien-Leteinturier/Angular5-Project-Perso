c30cb1dc68dc2296a63a662e7448872b1a7f4c0c
Leteinturier <adrienleteinturier@yahoo.fr>
2021-03-14 17:16:34 +0100

    chmod and change url for post mail

--- src/app/element-forms/element-forms.service.ts
+++ src/app/element-forms/element-forms.service.ts
@@ -12,7 +12,7 @@ export class ElementFormsService {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
-  postUrl:string = 'http://www.adrienleteinturier-webdev.fr/action';
+  postUrl:string = 'https://46ya6py434.execute-api.eu-west-1.amazonaws.com/latest/action';
 
 
 
